5b16fa7f4c6adeac6ff90603dc7ca29188154e62
Emily Thomas <ertgd8@mail.umkc.edu>
2015-04-25 19:11:50 -0500

    Completed Morse Tree Program
    Program works to encode and decode Morse code messages. It can probably
    be improved though.

--- Morse/Main.cpp
+++ Morse/Main.cpp
@@ -2,5 +2,8 @@
 
 int main(){
 	MorseTree tree;
+	tree.decode("_.. __.");
+	tree.encode("ac");
+	system("pause");
 	return 0;
 }
--- Morse/Tree.h
+++ Morse/Tree.h
@@ -10,36 +10,36 @@ struct MorseNode{
 	MorseNode* left;
 	MorseNode* right;
 	//Constructor
-	MorseNode(std::string the_let){
-		letter = the_let; // Letter is originally a dummy
+	MorseNode(){
+		letter = "*"; // Letter is originally a dummy
 		left = NULL;
 		right = NULL;
 	}
 	bool is_leaf(){ // Is this node a leaf?
-		if (left == NULL || right == NULL)
+		if (left == NULL && right == NULL)
 			return true;
 		else
 			return false;
 	}
+	//Destructor
+	~MorseNode() {}
 };
 
 class MorseTree{
 public:
 	MorseTree(){ 
 		build_map(); //get the map set up in the constructor
-		MorseNode r_node("*"); //set the root
-		root = &r_node;
+		root = new MorseNode;
 		build_tree();
 	} 
 	void build_map();
 	void build_tree();
-	//Returns true if successfully inserted
-	void insert(const std::string& key, const std::string& value);
 	void decode(const std::string& message);
-	void encode(const std::string& message);
-	void print(const std::string& message);
+	void encode(const std::string& message); // wrapper for search of letters in the tree to encode
+	std::string search(MorseNode* loc_root, std::string let, std::string str); //return value is only used inside the function
 
 private:
-	MorseNode* root;
-	std::map<std::string, std::string> code_map; 
+	MorseNode* root; 
+	std::map<std::string, std::string> code_map; //used for building the tree
+	std::string coded_let; //used for encoding a message
 };
